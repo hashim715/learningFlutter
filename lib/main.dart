e99c8da47ebbf06b0074914f5feb6c1ed657a6b2
hashim715 <mharus575@gmail.com>
2022-12-26 19:20:03 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,26 @@
 import 'package:flutter/material.dart';
+import "pages/home_page.dart";
+import 'pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
-      home: const HomePage(),
-    ),);  
-}
-
-class HomePage extends StatefulWidget{
-  const HomePage({Key? key}): super(key:key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-   
-  @override
-  void initState() {
-    _email=TextEditingController();
-    _password=TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title:const Text("Register")
-      ),
-      body:Column(
-        children:[
-          TextField(
-            controller: _email,
-            decoration: const InputDecoration(
-              hintText: "Enter your email"
-            ),
-          ),
-          TextField(
-            controller: _password,
-            decoration: const InputDecoration(
-              hintText: "Enter your Password"
-            ),
-          ),
-          TextButton(
-            onPressed: () async {},
-            child: const Text("Register"),
-          ),
-        ]
-      ),
-      drawer: Drawer(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/login": (context) => const HomePage(),
+        "/": (context) => LoginPage(),
+      },
     );
   }
 }
