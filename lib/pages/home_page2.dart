643c98aa7dd2efba1545e403a19327ee5d6fa58c
hashim715 <mharus575@gmail.com>
2023-01-05 19:32:53 +0500

    Matrial Drawer/DevTools/ListView/NetworkImage

---
+++ lib/pages/home_page2.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utils/routes.dart';
+import '../widgets/drawer.dart';
+
+class HomePage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home page"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to 30 days of flutter"),
+        ),
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
